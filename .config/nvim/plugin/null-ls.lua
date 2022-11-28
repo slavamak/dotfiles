6b1219cd623281c9fbc6e859adcd5d5c08c7c597
Slava Maksimov <hello@slavamak.com>
2022-11-28 05:05:41 +0300

    feat(nvim): update formatters

--- .config/nvim/plugin/null-ls.lua
+++ .config/nvim/plugin/null-ls.lua
@@ -14,10 +14,12 @@ end
 
 null_ls.setup {
   sources = {
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.completion.spell,
-    null_ls.builtins.diagnostics.eslint_d.with {
-      diagnostics_format = '[eslint] #{m}\n(#{c})',
-    },
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.stylelint,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.stylelint,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
   },
