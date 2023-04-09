3072797f17691b2a2ca15111850a06aeec5b270e
Slava Maksimov <hello@slavamak.com>
2023-04-09 22:54:10 +0300

    fix(tmux): issue with filename in toggle theme function

--- tmux/.config/tmux/toggle-theme.sh
+++ tmux/.config/tmux/toggle-theme.sh
@@ -8,10 +8,10 @@ current_mode_style=$(tmux show -Av mode-style)
 case $current_mode_style in
   $dark_mode_style)
     cp -rf "$HOME/.config/tmux/themes/tokyonight_day.conf" "$HOME/.config/tmux/theme.conf"
-    tmux "source $HOME/.tmux.conf"
+    tmux source "$HOME/.tmux.conf"
     ;;
   *)
     cp -rf "$HOME/.config/tmux/themes/tokyonight_night.conf" "$HOME/.config/tmux/theme.conf"
-    tmux "source $HOME/.tmux.conf"
+    tmux source "$HOME/.tmux.conf"
     ;;
 esac
