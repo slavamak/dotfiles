9b116c49930ed6277175e4f08e5dec2802b953b4
Slava Maksimov <hello@slavamak.com>
2023-04-10 19:56:20 +0300

    feat(tmux): change theme to github colors

--- tmux/.config/tmux/toggle-theme.sh
+++ tmux/.config/tmux/toggle-theme.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Toggle between light and dark themes
 
-light_mode_style="fg=#2e7de9,bg=#a8aecb"
-dark_mode_style="fg=#7aa2f7,bg=#3b4261"
+light_mode_style="fg=#24292f,bg=#f6f8fa"
+dark_mode_style="fg=#c9d1d9,bg=#1f2428"
 current_mode_style=$(tmux show -Av mode-style)
 
 case $current_mode_style in
   $dark_mode_style)
-    cp -rf "$HOME/.config/tmux/themes/tokyonight_day.conf" "$HOME/.config/tmux/theme.conf"
+    cp -rf "$HOME/.config/tmux/themes/github_light.conf" "$HOME/.config/tmux/theme.conf"
     tmux source "$HOME/.tmux.conf"
     ;;
   *)
-    cp -rf "$HOME/.config/tmux/themes/tokyonight_night.conf" "$HOME/.config/tmux/theme.conf"
+    cp -rf "$HOME/.config/tmux/themes/github_dark.conf" "$HOME/.config/tmux/theme.conf"
     tmux source "$HOME/.tmux.conf"
     ;;
 esac
