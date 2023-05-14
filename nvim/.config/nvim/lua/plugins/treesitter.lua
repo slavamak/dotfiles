c475f34b777816ee75cda0247f2e04e457d5614f
Slava Maksimov <hello@slavamak.com>
2023-05-14 15:29:45 +0300

    fix(nvim): treesitter vimdoc

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   opts = function(_, opts)
-    opts.ignore_install = { 'help' }
-
     vim.list_extend(opts.ensure_installed, {
       'dockerfile',
       'git_config',
       'jsdoc',
       'make',
       'toml',
-      'vimdoc',
     })
   end,
 }
