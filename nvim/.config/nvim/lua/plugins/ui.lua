959528b4a7ad87175ad1450dba5544c5bc20d2e2
Slava Maksimov <hello@slavamak.com>
2023-05-14 15:32:34 +0300

    feat(nvim): add config for lualine

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -105,4 +105,90 @@ return {
       },
     },
   },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = function(_, opts)
+      local icons = require('lazyvim.config').icons
+      local chevron_right = ' ' .. icons.ui.chevron_right .. ' '
+
+      opts.options = {
+        theme = 'auto',
+        globalstatus = true,
+        disabled_filetypes = {
+          statusline = { 'alpha' },
+          winbar = { 'neo-tree', 'alpha' },
+        },
+      }
+
+      opts.sections.lualine_c = {
+        {
+          'diagnostics',
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+      }
+
+      opts.sections.lualine_x = vim.list_extend(opts.sections.lualine_x, {
+        {
+          'filetype',
+          icons_enabled = false,
+        },
+      })
+
+      opts.winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+            separator = '',
+            padding = { left = 1, right = 0 },
+            fmt = function(path)
+              return vim.fs.dirname(path):gsub('/', chevron_right)
+                .. chevron_right
+            end,
+          },
+          {
+            'filetype',
+            icon_only = true,
+            separator = '',
+            padding = { left = 0, right = 1 },
+          },
+          {
+            'filename',
+            path = 0,
+            padding = { left = 0, right = 1 },
+            symbols = { modified = '  ', readonly = '', unnamed = '' },
+          },
+          {
+            function()
+              return require('nvim-navic').get_location()
+            end,
+            cond = function()
+              return package.loaded['nvim-navic']
+                and require('nvim-navic').is_available()
+            end,
+          },
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
+
+      opts.inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
+    end,
+  },
 }
