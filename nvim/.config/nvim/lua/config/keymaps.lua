b9b77570e65f85d5d93bd34bec159533384c1c78
Slava Maksimov <hello@slavamak.com>
2023-09-03 16:28:03 +0300

    feat(nvim): add keymaps for russian layout

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move the cursor using Russian layout
+vim.keymap.set('n', 'л', 'gk', opts)
+vim.keymap.set('n', 'о', 'gj', opts)
+vim.keymap.set('n', 'р', 'h', opts)
+vim.keymap.set('n', 'д', 'l', opts)
