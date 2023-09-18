8ef4834e937f1be3331f93ee6cccc5d8722aa0ab
Slava Maksimov <hello@slavamak.com>
2023-09-18 18:38:51 +0300

    feat(nvim): add keymaps and options

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,14 @@
-vim.keymap.set('n', 'л', 'gk')
-vim.keymap.set('n', 'о', 'gj')
-vim.keymap.set('n', 'р', 'h')
-vim.keymap.set('n', 'д', 'l')
+local bind = require('util').bind()
 
-vim.keymap.set('n', '[c', function()
+bind('n', '<leader>pv', vim.cmd.Ex, 'File Explorer')
+bind('i', 'jk', '<esc>', 'Escape')
+bind({ 'n', 'v', 'i' }, '<leader>qq', '<cmd>qa!<cr>', 'Exit Neovim')
+
+bind('n', 'л', 'gk', 'Up')
+bind('n', 'о', 'gj', 'Down')
+bind('n', 'р', 'h', 'Left')
+bind('n', 'д', 'l', 'Right')
+
+bind('n', '[c', function()
   require('treesitter-context').go_to_context()
-end)
+end, 'Jumping to context')
