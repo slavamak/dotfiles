e9f95f52353e7c598550705d192da6c5ead4f8ca
Slava Maksimov <hello@slavamak.com>
2023-09-18 18:38:51 +0300

    feat(nvim): add keymaps and options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.border_chars = 'rounded'
 
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
@@ -8,11 +12,10 @@ vim.opt.fileencoding = 'utf-8'
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.wrap = true
-vim.opt.colorcolumn = '100'
+vim.opt.colorcolumn = '80'
 vim.opt.termguicolors = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.breakindent = true
 vim.opt.breakindent = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth = 2
@@ -24,4 +27,8 @@ vim.opt.listchars = { space = '·', tab = '->' }
 vim.opt.showmatch = true
 vim.opt.cursorline = true
 vim.opt.hlsearch = true
-vim.opt.colorcolumn = '100'
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv 'HOME' .. '/.cache/nvim/undodir'
