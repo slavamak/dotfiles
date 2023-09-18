9d8d427eb596a70d0f238b6c54dd47547aad7bf8
Slava Maksimov <hello@slavamak.com>
2023-09-19 00:13:40 +0300

    feat(nvim): add which-key plugin

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -45,4 +45,14 @@ return {
     event = 'InsertEnter',
     config = true,
   },
+
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    config = true,
+  },
 }
