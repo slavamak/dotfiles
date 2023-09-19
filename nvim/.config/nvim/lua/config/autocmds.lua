84410ab9f901aacf03f60bcabb3d32bbe61fa3b9
Slava Maksimov <hello@slavamak.com>
2023-09-19 21:33:40 +0300

    feat(nvim): change lsp ui borders

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -17,3 +17,12 @@ vim.api.nvim_create_autocmd('FileType', {
   group = augroup 'transparent_cc',
   pattern = 'netrw',
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.api.nvim_set_hl(0, 'LspInfoBorder', { link = 'FloatBorder' })
+    vim.api.nvim_win_set_config(0, { border = vim.g.border_chars })
+  end,
+  group = augroup 'lsp_border',
+  pattern = { 'lspinfo', 'lsp-installer' },
+})
