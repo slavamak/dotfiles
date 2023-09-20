b76d367902db0f9a7d4ce8a380d90fea9f5fb5bb
Slava Maksimov <hello@slavamak.com>
2023-09-20 15:55:52 +0300

    feat(nvim): add keymaps

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -18,6 +18,26 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
     },
+    opts = function()
+      local actions = require 'telescope.actions'
+
+      return {
+        defaults = {
+          mappings = {
+            i = {
+              ['<Down>'] = actions.preview_scrolling_left,
+              ['<Up>'] = actions.preview_scrolling_right,
+              ['<C-j>'] = actions.move_selection_next,
+              ['<C-k>'] = actions.move_selection_previous,
+            },
+            n = {
+              ['<Down>'] = actions.preview_scrolling_left,
+              ['<Up>'] = actions.preview_scrolling_right,
+            },
+          },
+        },
+      }
+    end,
   },
 
   {
