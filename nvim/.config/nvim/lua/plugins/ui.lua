d8a2bbf47eb642e17cda60ef18be9d5ed0c1dca1
Slava Maksimov <hello@slavamak.com>
2023-10-01 16:22:01 +0300

    chore(nvim): remove unnecessary options

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -3,14 +3,6 @@ return {
     'projekt0n/github-nvim-theme',
     lazy = false,
     priority = 1000,
-    opts = {
-      styles = {
-        comments = 'NONE',
-        functions = 'NONE',
-        keywords = 'NONE',
-        variables = 'NONE',
-      },
-    },
     config = function()
       vim.cmd 'colorscheme github_light'
     end,
