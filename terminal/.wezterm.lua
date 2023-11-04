a6a5218d765610c5e89ae8b694a6e674a923d7e7
Slava Maksimov <hello@slavamak.com>
2023-11-04 20:51:02 +0300

    feat(terminal): change config for wezterm

--- terminal/.wezterm.lua
+++ terminal/.wezterm.lua
@@ -3,32 +3,13 @@ local wezterm = require 'wezterm'
 
 if wezterm.config_builder then config = wezterm.config_builder() end
 
-function get_appearance()
-  if wezterm.gui then return wezterm.gui.get_appearance() end
-  return 'Light'
-end
-
 function scheme_for_appearance(appearance)
   if appearance:find 'Dark' then return 'Tokyo Night' end
   return 'Github'
 end
 
-wezterm.on('window-config-reloaded', function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-  local appearance = window:get_appearance()
-  local scheme = scheme_for_appearance(appearance)
-  if overrides.color_scheme ~= scheme then
-    overrides.color_scheme = scheme
-    window:set_config_overrides(overrides)
-  end
-end)
-
-wezterm.on('update-right-status', function(window, pane)
-  window:set_right_status(window:active_workspace())
-end)
-
 config.animation_fps = 60
-config.color_scheme = scheme_for_appearance(get_appearance())
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font = wezterm.font {
   family = 'Iosevka Nerd Font',
   weight = 'Medium',
@@ -43,10 +24,10 @@ config.native_macos_fullscreen_mode = true
 config.scrollback_lines = 20000
 config.window_close_confirmation = 'NeverPrompt'
 config.window_padding = {
-  left = 4,
-  right = 4,
-  top = 4,
-  bottom = 4,
+  left = 16,
+  right = 16,
+  top = 16,
+  bottom = 16,
 }
 
 return config
